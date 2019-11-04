c9f2bf7b170e30e138d4f3ebaaf71fe43759947b
Victor <victor.otazu.estrada@sv.cmu.edu>
2019-11-03 20:00:11 -0800

    Improve the layout and add related videos

--- src/containers/VideoDetail/VideoDetail.jsx
+++ src/containers/VideoDetail/VideoDetail.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import './VideoDetail.scss';
 import {Video} from '../../components/Video/Video';
 import CommentList from "../../components/Comments/CommentList";
 import CommentForm from "../../components/Comments/CommentForm";
+import VideoThumbnail from '../../components/VideoThumbnail/VideoThumbnail';
 
 class VideoDetail extends Component {
     constructor(props) {
@@ -44,21 +46,44 @@ class VideoDetail extends Component {
     }
     render() {
         return(
-            <div class="video-detail">
-                <div style={{maxWidth: '80%'}}>
-                    <Video id='O2oZ2ucI1mA'/>
+            <div className="video-detail">
+                <div className="main">
+                    <div style={{maxWidth: '80%'}}>
+                        <Video
+                            filename='y2mate.com%20-%20how_to_do_a_presentation_5_steps_to_a_killer_opener_dEDcc0aCjaA_360p.mp4'/>
+                    </div>
+                    <div className="row">
+                        <div className="col-4 pt-3 border-right">
+                            <h3>Comments</h3>
+                            <CommentForm addComment={this.addComment}/>
+                        </div>
+                        <div className="col-8 pt-3 bg-white">
+                            <CommentList
+                                loading={this.state.loading}
+                                comments={this.state.comments}
+                            />
+                        </div>
+                    </div>
                 </div>
-                <div className="row">
-                    <div className="col-4  pt-3 border-right">
-                        <h6>Say something about my presentation</h6>
-                        <CommentForm addComment={this.addComment} />
-                    </div>
-                    <div className="col-8  pt-3 bg-white">
-                        <CommentList
-                            loading={this.state.loading}
-                            comments={this.state.comments}
-                        />
-                    </div>
+                <div className='side'>
+                    <VideoThumbnail uploader='Niro' createTime='2019-10-10' title='My first presentation video'
+                                    videoUrl='https://storage.cloud.google.com/pmock/y2mate.com%20-%20how_to_do_a_presentation_5_steps_to_a_killer_opener_dEDcc0aCjaA_360p.mp4?authuser=2&folder&organizationId'></VideoThumbnail>
+                    <VideoThumbnail uploader='Niro' createTime='2019-10-10' title='My first presentation video'
+                                    videoUrl='https://storage.cloud.google.com/pmock/y2mate.com%20-%20how_to_do_a_presentation_5_steps_to_a_killer_opener_dEDcc0aCjaA_360p.mp4?authuser=2&folder&organizationId'></VideoThumbnail>
+                    <VideoThumbnail uploader='Niro' createTime='2019-10-10' title='My first presentation video'
+                                    videoUrl='https://storage.cloud.google.com/pmock/y2mate.com%20-%20how_to_do_a_presentation_5_steps_to_a_killer_opener_dEDcc0aCjaA_360p.mp4?authuser=2&folder&organizationId'></VideoThumbnail>
+                    <VideoThumbnail uploader='Niro' createTime='2019-10-10' title='My first presentation video'
+                                    videoUrl='https://storage.cloud.google.com/pmock/y2mate.com%20-%20how_to_do_a_presentation_5_steps_to_a_killer_opener_dEDcc0aCjaA_360p.mp4?authuser=2&folder&organizationId'></VideoThumbnail>
+                    <VideoThumbnail uploader='Niro' createTime='2019-10-10' title='My first presentation video'
+                                    videoUrl='https://storage.cloud.google.com/pmock/y2mate.com%20-%20how_to_do_a_presentation_5_steps_to_a_killer_opener_dEDcc0aCjaA_360p.mp4?authuser=2&folder&organizationId'></VideoThumbnail>
+                    <VideoThumbnail uploader='Niro' createTime='2019-10-10' title='My first presentation video'
+                                    videoUrl='https://storage.cloud.google.com/pmock/y2mate.com%20-%20how_to_do_a_presentation_5_steps_to_a_killer_opener_dEDcc0aCjaA_360p.mp4?authuser=2&folder&organizationId'></VideoThumbnail>
+                    <VideoThumbnail uploader='Niro' createTime='2019-10-10' title='My first presentation video'
+                                    videoUrl='https://storage.cloud.google.com/pmock/y2mate.com%20-%20how_to_do_a_presentation_5_steps_to_a_killer_opener_dEDcc0aCjaA_360p.mp4?authuser=2&folder&organizationId'></VideoThumbnail>
+                    <VideoThumbnail uploader='Niro' createTime='2019-10-10' title='My first presentation video'
+                                    videoUrl='https://storage.cloud.google.com/pmock/y2mate.com%20-%20how_to_do_a_presentation_5_steps_to_a_killer_opener_dEDcc0aCjaA_360p.mp4?authuser=2&folder&organizationId'></VideoThumbnail>
+                    <VideoThumbnail uploader='Niro' createTime='2019-10-10' title='My first presentation video'
+                                    videoUrl='https://storage.cloud.google.com/pmock/y2mate.com%20-%20how_to_do_a_presentation_5_steps_to_a_killer_opener_dEDcc0aCjaA_360p.mp4?authuser=2&folder&organizationId'></VideoThumbnail>
                 </div>
             </div>
         );
