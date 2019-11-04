fee3ba7894a33f1572ba5253c9c76b72de63c360
Victor <victor.otazu.estrada@sv.cmu.edu>
2019-11-03 19:59:00 -0800

    Modify base url to support GC storage

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,13 +1,13 @@
 
 import React from 'react';
 
-const BASE_EMBED_URL = 'https://www.youtube.com/embed/';
+const BASE_EMBED_URL = 'https://storage.cloud.google.com/pmock/';
 
 export function Video(props) {
-    if (!props.id) {
+    if (!props.filename) {
         return null;
     }
-    const embedUrl = `${BASE_EMBED_URL}${props.id}`;
+    const embedUrl = `${BASE_EMBED_URL}${props.filename}?authuser=2&folder&organizationId`;
     return (
         <iframe width={'560'} height={'315'} src={embedUrl} frameBorder='0'
                 allow='autoplay; encrypted-media' allowFullScreen />
