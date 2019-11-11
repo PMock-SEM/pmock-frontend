2b632741f38665c8fc9449d831992a083d9c0dd6
陈淇格 <cqg186495@outlook.com>
2019-11-10 21:11:26 -0800

    integration of video detail page

--- src/components/Comments/CommentList.jsx
+++ src/components/Comments/CommentList.jsx
@@ -3,18 +3,15 @@ import Comment from "./Comment";
 
 class CommentList extends Component {
   render() {
+    const comments = this.props.comments;
     return (
       <div className="commentList">
-        <h5 className="text-muted mb-4">
-          <span className="badge badge-success">{this.props.comments.length}</span>
-          Comments
-        </h5>
         {
-          this.props.comments.length === 0 &&
+          comments.length === 0 &&
           (<div className="alert text-center alert-info">Be the first to comment</div>)
         }
-        {this.props.comments.map((comment, index) => (
-          <Comment key={index} comment={comment} />
+        {comments.map((comment, index) => (
+          <Comment key={index} content={comment.content} coachId={comment.coachId} />
         ))}
       </div>
     );
--- src/containers/VideoDetail/VideoDetail.jsx
+++ src/containers/VideoDetail/VideoDetail.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import config from '../../config';
 import './VideoDetail.scss';
 import Video from '../../components/Video/Video';
 import { connect } from 'react-redux';
@@ -8,30 +7,37 @@ import CommentForm from "../../components/Comments/CommentForm";
 import VideoThumbnail from '../../components/VideoThumbnail/VideoThumbnail';
 import { loadVideoDetail } from '../../actions/videoDetailAction';
 import { loadVideoListByUserId } from '../../actions/videoListAction';
-import { loadFeedbackListByVideoId, addFeedback } from '../../actions/feedbackListAction';
+import { loadFeedbackListByVideoId } from '../../actions/feedbackListAction';
+import { loadUserProfile } from '../../actions/userProfileAction';
+import Button from "../../components/Button/Button";
 import utils from '../../utils';
+import { NavLink } from 'react-router-dom';
 
 class VideoDetail extends Component {
   componentWillMount() {
     const videoId = this.props.match.params.id;
-    this.props.loadVideoDetail(videoId);
+    this.props.loadVideoDetail(videoId).then(() => {
+      this.props.loadVideoListByUserId(this.props.videoDetail.userId);
+      this.props.loadUserProfile(this.props.videoDetail.userId);
+    });
     this.props.loadFeedbackListByVideoId(videoId);
-    this.props.loadVideoListByUserId('5db74a7fd56be13c6a37f338');
   }
 
   render() {
     return (
       <div className="video-detail">
         <div className="main">
-          <div>
-            <Video videoId={this.props.videoDetail._id}
-              videoUrl={this.props.videoDetail.videoUrl}
-              videoName={this.props.videoDetail.videoName}
-              createTime={utils.convertTimeStamp(this.props.videoDetail.createdTime)} />
-          </div>
-          <div className="row">
-            <div className="col-4 pt-3 border-right">
-              <h3>Comments</h3>
+          <Video videoId={this.props.videoDetail._id}
+            videoUrl={this.props.videoDetail.videoUrl}
+            videoName={this.props.videoDetail.videoName}
+            createTime={utils.convertTimeStamp(this.props.videoDetail.createdTime)}
+            createHour={utils.convertTimeStampToHour(this.props.videoDetail.createdTime)} />
+          <div className="feedbacks">
+            <div className="feedback-form">
+              <div className="feedback-form-header">
+                <h3 className="feedback-form-title">Feedbacks</h3>
+                <Button className='invite-btn' text='Invite' />
+              </div>
               {this.props.auth ? <CommentForm /> : "you should login to comment"}
             </div>
             <div className="col-8 pt-3 bg-white">
@@ -40,10 +46,20 @@ class VideoDetail extends Component {
           </div>
         </div>
         <div className='side'>
+          <div className='user-thumbnail'>
+            <div className='user-avatar'>
+              <img className='user-avatar-image' alt='aaa' src={this.props.userProfile.avatarLink} ></img>
+            </div>
+            <NavLink className='user-name' to={`/users/${this.props.userProfile._id}`}>
+              <div className='user-first-name'>{this.props.userProfile.firstName}</div>
+              <div className='user-last-name'>{this.props.userProfile.lastName}</div>
+            </NavLink>
+          </div>
+          <div className='other-videos'>Other videos</div>
           {
             this.props.videoList.map(video => {
               return (
-                <VideoThumbnail uploader={video.userName} createTime={utils.convertTimeStamp(video.createdTime)} title={video.videoName} videoUrl={video.videoUrl} />
+                <VideoThumbnail uploader={video.userName} videoId={video._id} createTime={utils.convertTimeStamp(video.createdTime)} title={video.videoName} videoUrl={video.videoUrl} />
               );
             })
           }
@@ -55,14 +71,15 @@ class VideoDetail extends Component {
 
 
 const mapStateToProps = (state) => {
-  const { videoDetail, feedbackList, videoList, auth } = state;
-  return { videoDetail, feedbackList, videoList, auth };
+  const { videoDetail, feedbackList, videoList, auth, userProfile } = state;
+  return { videoDetail, feedbackList, videoList, auth, userProfile };
 };
 
 const mapDispatchToProps = {
   loadVideoDetail,
   loadFeedbackListByVideoId,
-  loadVideoListByUserId
+  loadVideoListByUserId,
+  loadUserProfile
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(VideoDetail);
