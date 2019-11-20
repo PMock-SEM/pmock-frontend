32068b167ee002d84455c512ab0287c5c42858d8
akritiaggarwal <akritia@andrew.cmu.edu>
2019-11-20 12:42:08 -0800

    adding gcloud changes

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
   'oauth_uri': 'https://www.linkedin.com/oauth/v2/authorization',
-  'api': 'http://localhost:3000',
+  'api': 'https://pmock-backend.appspot.com',
   'client_id': '86sbuleh3rid8d',
   'client_secret': 'kCpMKXFlPl6tmkGg',
   'based_embed_url': 'https://storage.cloud.google.com/pmock/'
